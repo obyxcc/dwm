34856e78ae1d372e398d9b3b1c91ebacce0d8e85
cole@obyx.cc <cole@obyx.cc>
2023-07-02 23:32:28 -0400

    added killunsel and added kill opts

--- dwm.c
+++ dwm.c
@@ -186,6 +186,9 @@ static void grabkeys(void);
 static void incnmaster(const Arg *arg);
 static void keypress(XEvent *e);
 static void killclient(const Arg *arg);
+static void killunsel(const Arg *arg);
+static void killallunsel(const Arg *arg);
+static void killall(const Arg *arg);
 static void manage(Window w, XWindowAttributes *wa);
 static void mappingnotify(XEvent *e);
 static void maprequest(XEvent *e);
@@ -1195,6 +1198,7 @@ keypress(XEvent *e)
 			keys[i].func(&(keys[i].arg));
 }
 
+// condense this TODO
 void
 killclient(const Arg *arg)
 {
@@ -1208,6 +1212,70 @@ killclient(const Arg *arg)
 		XSync(dpy, False);
 		XSetErrorHandler(xerror);
 		XUngrabServer(dpy);
+	}
+}
+
+void
+killunsel(const Arg *arg)
+{
+	Client *i = NULL;
+
+	if (!selmon->sel)
+		return;
+
+	for (i = selmon->clients; i; i = i->next) {
+		if (ISVISIBLE(i) && i != selmon->sel) {
+			if (!sendevent(i, wmatom[WMDelete])) {
+				XGrabServer(dpy);
+				XSetErrorHandler(xerrordummy);
+				XSetCloseDownMode(dpy, DestroyAll);
+				XKillClient(dpy, i->win);
+				XSync(dpy, False);
+				XSetErrorHandler(xerror);
+				XUngrabServer(dpy);
+			}
+		}
+	}
+}
+
+void
+killallunsel(const Arg *arg)
+{
+	Client *i = NULL;
+
+	if (!selmon->sel)
+		return;
+
+	for (i = selmon->clients; i; i = i->next) {
+		if (i != selmon->sel) {
+			if (!sendevent(i, wmatom[WMDelete])) {
+				XGrabServer(dpy);
+				XSetErrorHandler(xerrordummy);
+				XSetCloseDownMode(dpy, DestroyAll);
+				XKillClient(dpy, i->win);
+				XSync(dpy, False);
+				XSetErrorHandler(xerror);
+				XUngrabServer(dpy);
+			}
+		}
+	}
+}
+
+void
+killall(const Arg *arg)
+{
+	Client *i = NULL;
+
+	for (i = selmon->clients; i; i = i->next) {
+    if (!sendevent(i, wmatom[WMDelete])) {
+      XGrabServer(dpy);
+      XSetErrorHandler(xerrordummy);
+      XSetCloseDownMode(dpy, DestroyAll);
+      XKillClient(dpy, i->win);
+      XSync(dpy, False);
+      XSetErrorHandler(xerror);
+      XUngrabServer(dpy);
+    }
 	}
 }
 
