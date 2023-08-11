1744176315ed849910f198fb50881d65f3271826
cole@obyx.cc <cole@obyx.cc>
2023-08-11 08:29:57 -0400

    applied tagmonfixfs

--- dwm.c
+++ dwm.c
@@ -2601,9 +2601,17 @@ tag(const Arg *arg)
 void
 tagmon(const Arg *arg)
 {
-	if (!selmon->sel || !mons->next)
+	Client *c = selmon->sel;
+	if (!c || !mons->next)
 		return;
-	sendmon(selmon->sel, dirtomon(arg->i));
+	if (c->isfullscreen) {
+		c->isfullscreen = 0;
+		sendmon(c, dirtomon(arg->i));
+		c->isfullscreen = 1;
+		resizeclient(c, c->mon->mx, c->mon->my, c->mon->mw, c->mon->mh);
+		XRaiseWindow(dpy, c->win);
+	} else
+		sendmon(c, dirtomon(arg->i));
 	if (selmon->wfsymbol[0] == WFACTIVE)
 		focusmon(arg);
 }
